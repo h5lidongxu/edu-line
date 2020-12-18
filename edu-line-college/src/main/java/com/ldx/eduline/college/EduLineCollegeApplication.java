84dc7bfb6d2696ab8d108c3d00fde828754c8a35
lidongxu123 <2499493363@qq.com>
2020-12-18 17:21:15 +0800

    搭建

---
+++ edu-line-college/src/main/java/com/ldx/eduline/college/EduLineCollegeApplication.java
@@ -0,0 +1,13 @@
+package com.ldx.eduline.college;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduLineCollegeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduLineCollegeApplication.class, args);
+    }
+
+}
